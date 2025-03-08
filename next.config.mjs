415c5637550b020a62658b62d14350992502c78c
Anushka-tada <tadaanushka@gmail.com>
2025-03-08 12:48:43 +0530

    6th time commiting

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-  
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    trailingSlash: true, // Ensure kare ki static site sahi kaam kare
-  };
+const nextConfig = {};
+
+export default nextConfig;
+
+
   
-  export default nextConfig;
+
   
   
