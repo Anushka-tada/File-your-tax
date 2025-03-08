e2d39654d47f54748709e15ef948c9c3dfcc7fa3
Anushka-tada <tadaanushka@gmail.com>
2025-03-08 11:35:24 +0530

    third commit

--- src/app/services/authentication.service.js
+++ src/app/services/authentication.service.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 import { BASE_URL } from ".././utils/apibaseurlConfiguration";
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
-const getConfig = () => {
-  return {
-    headers: {
-      "Content-Type": "multipart/form-data",
-      Accept: "application/json",
-      Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
-    },
-  };
-};
+// const getConfig = () => {
+//   return {
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//       Accept: "application/json",
+//       Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+//     },
+//   };
+// };
 export const loginServ = async (formData) => {
   try {
     const response = await axios.post(BASE_URL + "user/login", formData);
