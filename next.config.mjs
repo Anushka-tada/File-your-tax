2205272d9f2d77895dd2b3f563a3bd06a3d56da9
Anushka-tada <tadaanushka@gmail.com>
2025-03-08 12:39:25 +0530

    Fourth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "standalone", // Ensure kare ki SSR aur API Routes sahi kaam karein
+  };
+  
+  export default nextConfig;
+  
